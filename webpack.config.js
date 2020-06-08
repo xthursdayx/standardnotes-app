ddd6ff06ac1e597283cea15860e67ee0be617e22
Karol Sójko <karolsojko@gmail.com>
2020-06-08 12:30:43 +0200

    Fix building dist process

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './app/assets/javascripts/index.js',
   output: {
-    filename: './javascripts/app.js'
+    filename: './javascripts/app.[contenthash].js'
   },
   devServer: {
     proxy: {
@@ -17,14 +20,31 @@ module.exports = {
     port: 3000
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
       __VERSION__: JSON.stringify(require('./package.json').version)
     }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
-      filename: './stylesheets/app.css',
+      filename: './stylesheets/app.[contenthash].css',
       ignoreOrder: false // Enable to remove warnings about conflicting order
-    })
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/favicon', to: 'favicon' },
+        { from: 'public/robots.txt', to: 'robots.txt' },
+        { from: 'public/404.html', to: '404.html' },
+        { from: 'public/422.html', to: '422.html' },
+        { from: 'public/500.html', to: '500.html' }
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.ejs',
+      inject: 'head',
+      SF_DEFAULT_SERVER: process.env.SF_DEFAULT_SERVER || 'https://sync.standardnotes.org',
+      EXTENSIONS_MANAGER_LOCATION: process.env.EXTENSIONS_MANAGER_LOCATION || 'public/extensions/extensions-manager/dist/index.html',
+      BATCH_MANAGER_LOCATION: process.env.BATCH_MANAGER_LOCATION || 'public/extensions/batch-manager/dist/index.min.html'
+    }),
   ],
   devtool: 'source-map',
   resolve: {
